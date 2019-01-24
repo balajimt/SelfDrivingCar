2d0d33e54ca9dbd77f3cc855d8761b2aae3bef7b
Balaji Muthazhagan <balajimuthazhagan@gmail.com>
2019-01-24 09:13:44 +0530

    Initial Commit

---
+++ Trainer.py
@@ -0,0 +1,53 @@
+import os
+import tensorflow as tf
+from tensorflow.core.protobuf import saver_pb2
+import DrivingData
+import model
+
+LOGDIR = './save'
+
+sess = tf.InteractiveSession()
+
+L2NormConst = 0.001
+
+train_vars = tf.trainable_variables()
+
+loss = tf.reduce_mean(tf.square(tf.subtract(model.y_, model.y))) + tf.add_n([tf.nn.l2_loss(v) for v in train_vars]) * L2NormConst
+train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
+sess.run(tf.initialize_all_variables())
+
+# create a summary to monitor cost tensor
+tf.summary.scalar("loss", loss)
+# merge all summaries into a single op
+merged_summary_op =  tf.summary.merge_all()
+
+saver = tf.train.Saver(write_version = saver_pb2.SaverDef.V1)
+
+# op to write logs to Tensorboard
+logs_path = './logs'
+summary_writer = tf.summary.FileWriter(logs_path, graph=tf.get_default_graph())
+
+noOfEpochs = 30
+batchSize = 100
+
+# train over the dataset about 30 times
+for epoch in range(noOfEpochs):
+  for i in range(int(DrivingData.num_images/batchSize)):
+    xs, ys = DrivingData.loadTrainData(batchSize)
+    train_step.run(feed_dict={model.x: xs, model.y_: ys, model.keep_prob: 0.8})
+    if i % 10 == 0:
+      xs, ys = DrivingData.loadValidationData(batchSize)
+      loss_value = loss.eval(feed_dict={model.x:xs, model.y_: ys, model.keep_prob: 1.0})
+      print("Epoch: %d, Step: %d, Loss: %g" % (epoch, epoch * batchSize + i, loss_value))
+
+    # write logs at every iteration
+    summary = merged_summary_op.eval(feed_dict={model.x:xs, model.y_: ys, model.keep_prob: 1.0})
+    summary_writer.add_summary(summary, epoch * DrivingData.num_images/batchSize + i)
+
+    if i % batchSize == 0:
+      if not os.path.exists(LOGDIR):
+        os.makedirs(LOGDIR)
+      checkpoint_path = os.path.join(LOGDIR, "model.ckpt")
+      filename = saver.save(sess, checkpoint_path)
+  print("Model saved in file: %s" % filename)
+
