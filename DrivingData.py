0cd392a0959bce00f7d9e664f17fe726e2137425
Balaji Muthazhagan <balajimuthazhagan@gmail.com>
2019-01-24 19:11:31 +0530

    Added COnfigurations

--- DrivingData.py
+++ DrivingData.py
@@ -1,8 +1,8 @@
 import scipy.misc
 import random
 
-xs = []
-ys = []
+xColumnDataset = []
+yColumnDataset = []
 
 #points to the end of the last batch
 train_batch_pointer = 0
@@ -11,21 +11,21 @@ val_batch_pointer = 0
 #read data.txt
 with open("DrivingDataset/data.txt") as f:
     for line in f:
-        xs.append("DrivingDataset/" + line.split()[0])
+        xColumnDataset.append("DrivingDataset/" + line.split()[0])
         #the paper by Nvidia uses the inverse of the turning radius,
         #but steering wheel angle is proportional to the inverse of turning radius
         #so the steering wheel angle in radians is used as the output
-        ys.append(float(line.split()[1]) * scipy.pi / 180)
+        yColumnDataset.append(float(line.split()[1]) * scipy.pi / 180)
 
 #get number of images
-num_images = len(xs)
+noOfImages = len(xColumnDataset)
 
 
-train_xs = xs[:int(len(xs) * 0.8)]
-train_ys = ys[:int(len(xs) * 0.8)]
+train_xs = xColumnDataset[:int(len(xColumnDataset) * 0.8)]
+train_ys = yColumnDataset[:int(len(xColumnDataset) * 0.8)]
 
-val_xs = xs[-int(len(xs) * 0.2):]
-val_ys = ys[-int(len(xs) * 0.2):]
+val_xs = xColumnDataset[-int(len(xColumnDataset) * 0.2):]
+val_ys = yColumnDataset[-int(len(xColumnDataset) * 0.2):]
 
 num_train_images = len(train_xs)
 num_val_images = len(val_xs)
@@ -34,9 +34,9 @@ def loadTrainData(batch_size):
     global train_batch_pointer
     x_out = []
     y_out = []
-    for i in range(0, batch_size):
-        x_out.append(scipy.misc.imresize(scipy.misc.imread(train_xs[(train_batch_pointer + i) % num_train_images])[-150:], [66, 200]) / 255.0)
-        y_out.append([train_ys[(train_batch_pointer + i) % num_train_images]])
+    for counterVariable in range(0, batch_size):
+        x_out.append(scipy.misc.imresize(scipy.misc.imread(train_xs[(train_batch_pointer + counterVariable) % num_train_images])[-150:], [66, 200]) / 255.0)
+        y_out.append([train_ys[(train_batch_pointer + counterVariable) % num_train_images]])
     train_batch_pointer += batch_size
     return x_out, y_out
 
@@ -44,8 +44,8 @@ def loadValidationData(batch_size):
     global val_batch_pointer
     x_out = []
     y_out = []
-    for i in range(0, batch_size):
-        x_out.append(scipy.misc.imresize(scipy.misc.imread(val_xs[(val_batch_pointer + i) % num_val_images])[-150:], [66, 200]) / 255.0)
-        y_out.append([val_ys[(val_batch_pointer + i) % num_val_images]])
+    for counterVariable in range(0, batch_size):
+        x_out.append(scipy.misc.imresize(scipy.misc.imread(val_xs[(val_batch_pointer + counterVariable) % num_val_images])[-150:], [66, 200]) / 255.0)
+        y_out.append([val_ys[(val_batch_pointer + counterVariable) % num_val_images]])
     val_batch_pointer += batch_size
     return x_out, y_out
--- Trainer.py
+++ Trainer.py
@@ -6,7 +6,7 @@ import model
 
 LOGDIR = './save'
 
-sess = tf.InteractiveSession()
+tensorflowSession = tf.InteractiveSession()
 
 L2NormConst = 0.001
 
@@ -14,14 +14,14 @@ train_vars = tf.trainable_variables()
 
 loss = tf.reduce_mean(tf.square(tf.subtract(model.y_, model.y))) + tf.add_n([tf.nn.l2_loss(v) for v in train_vars]) * L2NormConst
 train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
-sess.run(tf.initialize_all_variables())
+tensorflowSession.run(tf.initialize_all_variables())
 
 # create a summary to monitor cost tensor
 tf.summary.scalar("loss", loss)
 # merge all summaries into a single op
 merged_summary_op =  tf.summary.merge_all()
 
-saver = tf.train.Saver(write_version = saver_pb2.SaverDef.V1)
+savedWeightSession = tf.train.Saver(write_version = saver_pb2.SaverDef.V1)
 
 # op to write logs to Tensorboard
 logs_path = './logs'
@@ -32,22 +32,22 @@ batchSize = 100
 
 # train over the dataset about 30 times
 for epoch in range(noOfEpochs):
-  for i in range(int(DrivingData.num_images/batchSize)):
-    xs, ys = DrivingData.loadTrainData(batchSize)
-    train_step.run(feed_dict={model.x: xs, model.y_: ys, model.keep_prob: 0.8})
-    if i % 10 == 0:
-      xs, ys = DrivingData.loadValidationData(batchSize)
-      loss_value = loss.eval(feed_dict={model.x:xs, model.y_: ys, model.keep_prob: 1.0})
-      print("Epoch: %d, Step: %d, Loss: %g" % (epoch, epoch * batchSize + i, loss_value))
+  for counterVariable in range(int(DrivingData.noOfImages/batchSize)):
+    xColumnDataset, yColumnDataset = DrivingData.loadTrainData(batchSize)
+    train_step.run(feed_dict={model.x: xColumnDataset, model.y_: yColumnDataset, model.keep_prob: 0.8})
+    if counterVariable % 10 == 0:
+      xColumnDataset, yColumnDataset = DrivingData.loadValidationData(batchSize)
+      loss_value = loss.eval(feed_dict={model.x:xColumnDataset, model.y_: yColumnDataset, model.keep_prob: 1.0})
+      print("Epoch: %d, Step: %d, Loss: %g" % (epoch, epoch * batchSize + counterVariable, loss_value))
 
     # write logs at every iteration
-    summary = merged_summary_op.eval(feed_dict={model.x:xs, model.y_: ys, model.keep_prob: 1.0})
-    summary_writer.add_summary(summary, epoch * DrivingData.num_images/batchSize + i)
+    summary = merged_summary_op.eval(feed_dict={model.x:xColumnDataset, model.y_: yColumnDataset, model.keep_prob: 1.0})
+    summary_writer.add_summary(summary, epoch * DrivingData.noOfImages/batchSize + counterVariable)
 
-    if i % batchSize == 0:
+    if counterVariable % batchSize == 0:
       if not os.path.exists(LOGDIR):
         os.makedirs(LOGDIR)
       checkpoint_path = os.path.join(LOGDIR, "model.ckpt")
-      filename = saver.save(sess, checkpoint_path)
+      filename = savedWeightSession.save(tensorflowSession, checkpoint_path)
   print("Model saved in file: %s" % filename)
 
