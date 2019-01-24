7e893d0817f05d40430e8cf8613e38214dd98c84
Balaji Muthazhagan <balajimuthazhagan@gmail.com>
2019-01-24 19:39:16 +0530

    Removed redundant lines from Main application

--- SteeringAnglePredictor.py
+++ SteeringAnglePredictor.py
@@ -31,17 +31,17 @@ while(cv2.waitKey(10) != ord('q')):
     # Predicted angle is got from the Model's predictions
     predictedAngle = model.y.eval(feed_dict={model.x: [grayscaleImageData], model.keep_prob: 1.0})[0][0] * 180.0 / math.pi
     
-	# Writing to logs
+    # Writing to logs
     with open('logs//'+timestr+'.csv','a') as fd:
         fd.write(str(predictedAngle)+","+str(yColumnDataset[counterVariable]*180/math.pi)+"\n")
     
-	# Showing the video frame and steering wheel on screen
+    # Showing the video frame and steering wheel on screen
     cv2.imshow("RoadView", cv2.cvtColor(colorImageData, cv2.COLOR_RGB2BGR))
 	
     steeringAngle += 0.2 * pow(abs((predictedAngle - steeringAngle)), 2.0 / 3.0) * (predictedAngle - steeringAngle) / abs(predictedAngle - steeringAngle)
     steeringWheelImageRotation = cv2.getRotationMatrix2D((swImageCols/2,swImageRows/2),-steeringAngle,1)
     
-	steeringWheelImage = cv2.warpAffine(steeringImage,steeringWheelImageRotation,(swImageCols,swImageRows))
+    steeringWheelImage = cv2.warpAffine(steeringImage,steeringWheelImageRotation,(swImageCols,swImageRows))
     cv2.imshow("Steering Wheel Angle", steeringWheelImage)
     counterVariable += 1
 	
